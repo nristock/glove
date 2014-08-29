6ef8eea49f566c059d70cc55e711d3467aefb7c8
Monofraps <monofraps@gmail.com>
2014-08-29 15:37:00 +0200

    Removed python core lib from main repo into deps repo Updated get-deps scripts

---
+++ get-deps-python.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone -b python --single-branch git@github.com:Monofraps/glove-deps.git python
+
---
+++ get-deps-win.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git clone -b win64 --single-branch git@github.com:Monofraps/glove-deps.git deps
