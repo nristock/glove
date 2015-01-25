15e18f6ca2912053d1d4d5a42d017b8e2b9f5901
Monofraps <monofraps@gmail.com>
2015-01-25 01:29:50 +0100

    Fixed weird issue with boost::format not displaying yaml converter exceptions

--- src/core/GloveException.cc
+++ src/core/GloveException.cc
@@ -15,6 +15,9 @@ GloveException::~GloveException() {
 }
 
 const char* GloveException::what() const throw() {
+    // The following line is - for some weird reason I can't explain - required because yaml-cpp converter exception
+    // messages won't render without it.
+    (boost::format("%1%\n\tin %2%:%3%") % message % file % line);
     return (boost::format("%1%\n\tin %2%:%3%") % message % file % line).str().c_str();
 }
 
