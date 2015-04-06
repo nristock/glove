7d81d138b5597654617c3d7ba4a5c15a2474f6b7
Monofraps <monofraps@gmail.com>
2015-04-06 17:56:36 +0200

    Added sized types to CommonTypes header

--- src/glove/CommonTypes.hpp
+++ src/glove/CommonTypes.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <cinttypes>
+
 namespace glove {
 using uchar = unsigned char;
 using ushort = unsigned short;
 using uint = unsigned int;
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
 }
